58d869afed2a7d77e1851b9900e70f932e19e081
sanyajain <f2013188@pilani.bits-pilani.ac.in>
2015-06-23 23:43:45 +0530

    Start Screen and the Clickable Map
    Both start screen and map screen have been added with active links for
    buttons where functionalities are defined

---
+++ MapScreen.swift
@@ -0,0 +1,28 @@
+//
+//  MapScreen.swift
+//  Database
+//
+//  Created by Sanya Jain on 22/06/15.
+//  Copyright (c) 2015 Sanya Jain. All rights reserved.
+//
+
+import UIKit
+
+class MapScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func ClickableMap(sender: UIButton) {
+    }
+    
+   
+}
---
+++ StartScreen.swift
@@ -0,0 +1,32 @@
+//
+//  StartScreen.swift
+//  Database
+//
+//  Created by Sanya Jain on 22/06/15.
+//  Copyright (c) 2015 Sanya Jain. All rights reserved.
+//
+
+import UIKit
+
+class StartScreen: UIViewController {
+    
+    
+    @IBOutlet weak var PowerUp: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Start(sender: UIButton) {
+    }
+
+    
+}
