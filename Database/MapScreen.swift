fbd336d42d7f46651665edcad2c52b2914d9bca5
Andrew H <crazy4sb@gmail.com>
2015-12-07 17:51:22 -0500

    Allow two clicks on level 1. Also changed the push segues returning to the MapScreen into unwind segues (in storyboard) -- this induces a "pop" instead of "push". This way, there will only be one MapScreen instance in the whole program, instead of a new instance being created every time.

--- Database/MapScreen.swift
+++ Database/MapScreen.swift
@@ -11,18 +11,20 @@ class MapScreen: UIViewController {
     
     
     var numberToDisplay = 0
+    var timesPlayed1 = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Back Button of navigation controller hidden
         self.navigationItem.setHidesBackButton(true, animated:true);
-
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         // setting the orientation to portrait
         let value = UIInterfaceOrientation.Portrait.rawValue
         UIDevice.currentDevice().setValue(value, forKey: "orientation")
-        
-        
     }
 
     //Level 2 Button: clickable
@@ -41,7 +43,7 @@ class MapScreen: UIViewController {
         
       // Testing condition if level 1 button pressed again after comming out of the scenario
         
-        if (numberToDisplay > 0)
+        if (timesPlayed1 > 1)
         {
             print("This action is not possible!! Kindly choose another level!!")
             
@@ -49,7 +51,7 @@ class MapScreen: UIViewController {
             let alertView = UIAlertView();
             alertView.addButtonWithTitle("Ok");
             alertView.title = "MESSAGE!!!";
-            alertView.message = "You have already played this scenario! Go try another level!!";
+            alertView.message = "You have already played this scenario a few times! Go try another level!!";
             
             alertView.show();
             
@@ -58,7 +60,7 @@ class MapScreen: UIViewController {
             
             // condition for first time click - navigates to scenario
         else{
-            
+            timesPlayed1++
             performSegueWithIdentifier("start1View", sender: self)
             
             
@@ -66,6 +68,10 @@ class MapScreen: UIViewController {
         
         
        
+    }
+    
+    @IBAction func unwindToMapScreen(segue: UIStoryboardSegue) {
+        print("Unwind to map screen")
     }
    
 }
