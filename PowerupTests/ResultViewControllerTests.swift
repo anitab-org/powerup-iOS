b19eb4cb88ad235f95271bc30019ec8f7c2a43fd
erikaaatan <34075473+erikaaatan@users.noreply.github.com>
2018-01-27 20:51:38 -0500

    Add unit tests (#197)
    Unit tests have been added for MapViewController, CustomizeAvatarViewController, MinesweeperGameScene, ResultViewController, ShopViewController, SinkToSwimGameScene, and VocabMatchingGameScene.

--- PowerupTests/ResultViewControllerTests.swift
+++ PowerupTests/ResultViewControllerTests.swift
@@ -37,8 +37,8 @@ class ResultViewControllerTests: XCTestCase {
     }
     
     /**
-      - Test that ResultViewController correctly saved and fetched the score
-    */
+     - Test that ResultViewController correctly saved and fetched the score
+     */
     func testScoreFetchingAndSaving() {
         let originalScore = Score(karmaPoints: 31, healing: 30, strength: 20, telepathy: 10, invisibility: 40)
         
@@ -57,5 +57,41 @@ class ResultViewControllerTests: XCTestCase {
         
         // Score is correct.
         XCTAssertEqual(Int(showedScore!), originalScore.karmaPoints + resultViewController.karmaGain)
+    }
+    
+    /**
+     - Test that the labels for completed scenario and karma points are accurate
+     */
+    func testLabelsHaveCorrectContent() {
+        let score = Score(karmaPoints: 10, healing: 10, strength: 10, telepathy: 10, invisibility: 10)
+        
+        // Given
+        let mockSource = MockSource(score: score)
+        resultViewController.dataSource = mockSource
+        resultViewController.completedScenarioName = "School"
+        
+        // When
+        resultViewController.viewDidLoad()
+        
+        // Then
+        XCTAssertEqual(resultViewController.scenarioName.text, "Current Scenario: " + resultViewController.completedScenarioName)
+        XCTAssertEqual(resultViewController.karmaPointsLabel.text, String(score.karmaPoints))
+    }
+    
+    /**
+     - Test that completing a scenario adds 20 karma points to the database.
+     */
+    func testGainKarmaPoints() {
+        let score = Score(karmaPoints: 0, healing: 15, strength: 20, telepathy: 10, invisibility: 20)
+        
+        // Given
+        let mockSource = MockSource(score: score)
+        resultViewController.dataSource = mockSource
+        
+        // When
+        resultViewController.gainKarmaPoints()
+        
+        // Then
+        XCTAssertEqual(resultViewController.karmaPointsLabel.text, String(20))
     }
 }
