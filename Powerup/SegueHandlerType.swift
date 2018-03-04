dc800aa6b15a38c5c391af633f8043158278fe44
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-03-04 18:31:09 +0530

    reindenting SegueHandlerType.swift

--- Powerup/SegueHandlerType.swift
+++ Powerup/SegueHandlerType.swift
@@ -8,8 +8,8 @@ protocol SegueHandlerType {
 extension SegueHandlerType where Self: UIViewController,
     SegueIdentifier.RawValue == String
 {
-    func performSegueWithIdentifier(_ segueIdentifier: SegueIdentifier,
-                                    sender: AnyObject?) {
+    
+    func performSegueWithIdentifier(_ segueIdentifier: SegueIdentifier,sender: AnyObject?) {
         
         performSegue(withIdentifier: segueIdentifier.rawValue, sender: sender)
     }
