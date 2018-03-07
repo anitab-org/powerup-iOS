165908377ecf49abaa35089869bdcee99778e1aa
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-03-07 21:45:40 +0530

    reindenting SegueHandlerType.swift

--- Powerup/SegueHandlerType.swift
+++ Powerup/SegueHandlerType.swift
@@ -5,21 +5,21 @@ protocol SegueHandlerType {
     associatedtype SegueIdentifier: RawRepresentable
 }
 
-extension SegueHandlerType where Self: UIViewController,SegueIdentifier.RawValue == String {
-        func performSegueWithIdentifier(_ segueIdentifier: SegueIdentifier,sender: AnyObject?) {
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+    func performSegueWithIdentifier(_ segueIdentifier: SegueIdentifier, sender: AnyObject?) {
         performSegue(withIdentifier: segueIdentifier.rawValue, sender: sender)
     }
-    
+
     func segueIdentifierForSegue(_ segue: UIStoryboardSegue) -> SegueIdentifier? {
-        if segue.identifier==nil
-        {
+        if segue.identifier == nil
+            {
             return nil
         }
-        else {
-            guard let identifier = segue.identifier,
-                let segueIdentifier = SegueIdentifier(rawValue: identifier) else { fatalError("Invalid segue identifier \(segue.identifier).") }
-            return segueIdentifier
+            else {
+                guard let identifier = segue.identifier,
+                    let segueIdentifier = SegueIdentifier(rawValue: identifier) else { fatalError("Invalid segue identifier \(segue.identifier).") }
+                return segueIdentifier
         }
     }
-    
+
 }
