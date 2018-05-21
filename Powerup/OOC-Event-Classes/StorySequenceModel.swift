ac01c10196c8cc569f0b7cac026ec51ade5141c7
just-cady <cady@notnatural.co>
2018-05-21 18:08:02 -0500

    Added models for handling story sequence events

--- Powerup/OOC-Event-Classes/StorySequenceModel.swift
+++ Powerup/OOC-Event-Classes/StorySequenceModel.swift
@@ -7,3 +7,41 @@
 //
 
 import UIKit
+
+enum StorySequenceImagePosition {
+    case hidden
+    case start
+    case mid
+    case far
+    case dismiss
+}
+
+struct StorySequenceEvent {
+    var text: String?,
+    image: String?,
+    position: StorySequenceImagePosition?
+    
+    init (txt: String?, img: String?, pos: StorySequenceImagePosition?) {
+        self.text = txt
+        self.image = img
+        self.position = pos
+    }
+}
+
+struct StorySequenceStep {
+    var lEvent: StorySequenceEvent?,
+    rEvent: StorySequenceEvent?
+    
+    init (lEvent: StorySequenceEvent?, rEvent: StorySequenceEvent?) {
+        self.lEvent = lEvent
+        self.rEvent = rEvent
+    }
+}
+
+struct StorySequence {
+    var steps: Dictionary<Int, StorySequenceStep>
+    
+    init (_ steps: Dictionary<Int, StorySequenceStep>) {
+        self.steps = steps
+    }
+}
--- Powerup/OOC-Event-Classes/StorySequences.swift
+++ Powerup/OOC-Event-Classes/StorySequences.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+
 /**
  Defined in StorySequence.swift. Called in ScenarioViewController.startSequence()
  
@@ -14,6 +15,44 @@ import Foundation
  
  Each key is the ScenarioID, the value is a StorySequence.
  */
-let introStorySequences = [
-    5: "home"
+let introStorySequences: Dictionary<Int, StorySequence> = [
+    5: home
 ]
+
+// MARK: Scenario 5 - Home
+// define each story sequence as a private let, named descriptively
+// a StorySequence is a dictionary of StorySequenceStep, keys must be Int and serve to make the collection more readable as well as giving better access to starting a sequence in the middle
+// a StorySequenceStep describes two StorySequenceEvent, a left event and a right event.
+// A StorySequenceEvent describes the text, image, and image position.
+// if any properties are left nil, there will be no change from the previous step
+// you can nil the entire event as well (for no change from the previous step)
+// calling .dismiss in the pos property hides *and* releases the image from the imageView
+private let home: StorySequence =
+    StorySequence(
+        [
+            0: StorySequenceStep(lEvent: StorySequenceEvent(txt: "left text event, step 0",
+                                                            img: "test_image_normal",
+                                                            pos: .start),
+                                 rEvent: nil
+            ),
+            1: StorySequenceStep(lEvent: nil,
+                                 rEvent: StorySequenceEvent(txt: nil,
+                                                            img: nil,
+                                                            pos: nil)
+            ),
+            2: StorySequenceStep(lEvent: StorySequenceEvent(txt: "left text event, step 0",
+                                                            img: "test_image_normal",
+                                                            pos: .start),
+                                 rEvent: StorySequenceEvent(txt: nil,
+                                                            img: nil,
+                                                            pos: nil)
+            ),
+            3: StorySequenceStep(lEvent: StorySequenceEvent(txt: "left text event, step 0",
+                                                            img: "test_image_normal",
+                                                            pos: .start),
+                                 rEvent: StorySequenceEvent(txt: nil,
+                                                            img: nil,
+                                                            pos: nil)
+            )
+        ]
+    )
