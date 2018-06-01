6097d85b1cb8594f2764869d6ef45c9b165636ad
just-cady <cady@notnatural.co>
2018-06-01 13:49:56 -0500

    Added file PopupEventPlayerTests

---
+++ PowerupUITests/PopupEventPlayerTests.swift
@@ -0,0 +1,27 @@
+//
+//  PopupEventPlayerTests.swift
+//  PowerupUITests
+//
+//  Created by KD on 6/1/18.
+//  Copyright © 2018 Systers. All rights reserved.
+//
+
+import XCTest
+
+class PopupEventPlayerTests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+    }
+    
+}
