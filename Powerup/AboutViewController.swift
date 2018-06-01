c3b56dcf9c3ff20d44332788861854466b280729
just-cady <cady@notnatural.co>
2018-06-01 14:10:13 -0500

    Added a popup to the about view to speed up ui testing

--- Powerup/AboutViewController.swift
+++ Powerup/AboutViewController.swift
@@ -1,9 +1,26 @@
 import UIKit
 
-class AboutViewController: UIViewController {
-    
+class AboutViewController: UIViewController, PopupEventPlayerDelegate {
+
     // MARK: Action
     @IBAction func homeButtonTouched(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        addPopup()
+    }
+
+    func addPopup() {
+        let model = PopupEvent(topText: "Made with ♥",
+                               botText: "by Systers Open Source",
+                               imgName: nil,
+                               doSound: false)
+        let popup: PopupEventPlayer = PopupEventPlayer(delegate: self, model: model)
+        self.view.addSubview(popup)
+    }
+
+    func popupDidFinish(sender: PopupEventPlayer) {
+        sender.removeFromSuperview()
     }
 }
