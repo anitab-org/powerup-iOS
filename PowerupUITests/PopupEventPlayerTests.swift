3cad2cbca0bb50bd7342d9232fa5eb205705f133
justKD <kd@notnatural.co>
2018-06-07 15:27:10 -0500

    Added ui tests for StorySequencePlayer (#283)

--- PowerupUITests/PopupEventPlayerTests.swift
+++ PowerupUITests/PopupEventPlayerTests.swift
@@ -66,19 +66,3 @@ class PopupEventPlayerTests: XCTestCase {
     }
 
 }
-
-// a manual wait function to be able to check that a popup is dismissed automatically
-extension XCTestCase {
-
-    func wait(for duration: TimeInterval) {
-        let waitExpectation = expectation(description: "Waiting")
-
-        let when = DispatchTime.now() + duration
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            waitExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: duration)
-    }
-
-}
