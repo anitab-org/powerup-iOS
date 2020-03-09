a3e06768d8252fa923dfcae6503c9eebbeb3e89b
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-03-09 23:50:55 +0530

    Story Sequence margin

--- Powerup/OOC-Event-Classes/StorySequencePlayer.swift
+++ Powerup/OOC-Event-Classes/StorySequencePlayer.swift
@@ -89,7 +89,7 @@ class StorySequencePlayer: UIView {
         super.init(frame: frame)
 
         self.accessibilityIdentifier = AccessibilityIdentifiers.storySequencePlayer.rawValue
-        let margin: CGFloat = 25
+        let margin: CGFloat = 25 // margin for sentances
         let imageViewHeight: CGFloat = 0.6
 
         addBlur(self, .dark)
