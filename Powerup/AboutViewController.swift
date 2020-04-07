4b140095edb7db15c766653e194acbcda8447cbc
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-04-07 17:29:48 +0530

    About view new update

--- Powerup/AboutViewController.swift
+++ Powerup/AboutViewController.swift
@@ -7,10 +7,25 @@ class AboutViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBOutlet weak var aboutViewText: UITextView!
+
+    
     override func viewDidAppear(_ animated: Bool) {
         addPopup()
+        text(desc: aboutText)
+        
     }
 
+    func text(desc: String){
+        aboutViewText.text! = desc
+        aboutViewText.textColor? = #colorLiteral(red: 0.3803921569, green: 0.6, blue: 0.6928837435, alpha: 1)
+        aboutViewText.safeAreaInsets.left
+        aboutViewText.safeAreaInsets.right
+        aboutViewText.safeAreaInsets.bottom
+        aboutViewText.font = UIFont(name: "Arial", size: 20)
+        
+    }
+    
     func addPopup() {
         let model = PopupEvent(topText: "Made with ♥",
                                botText: "by Systers Open Source",
