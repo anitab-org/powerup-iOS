1dbf39430ad91ca37144222b625b1373c9e300d0
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-04-17 03:54:29 +0530

    Update attributes

--- Powerup/AboutViewController.swift
+++ Powerup/AboutViewController.swift
@@ -7,23 +7,39 @@ class AboutViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
-    @IBOutlet weak var aboutViewText: UITextView!
 
+    @IBOutlet weak var label1: UILabel!
+ 
+    @IBOutlet weak var answerLabel1: UILabel!
+    
+    @IBOutlet weak var answerLabel2: UILabel!
+    
+    @IBOutlet weak var answerLabel3: UILabel!
     
     override func viewDidAppear(_ animated: Bool) {
         addPopup()
-        text(desc: aboutText)
+       // text(desc1: aboutText, desc2: aboutText2, desc3: aboutText3)
+        text(desc1: aboutText, desc2: aboutText2, desc3: aboutText3)
         
     }
 
-    func text(desc: String){
-        aboutViewText.text! = desc
-        aboutViewText.textColor? = #colorLiteral(red: 0.3803921569, green: 0.6, blue: 0.6928837435, alpha: 1)
-        aboutViewText.safeAreaInsets.left
-        aboutViewText.safeAreaInsets.right
-        aboutViewText.safeAreaInsets.bottom
-        aboutViewText.font = UIFont(name: "Arial", size: 20)
-        
+    func text(desc1: String, desc2: String, desc3: String){
+       label1.text! = "The Game"
+        label1.textColor? = #colorLiteral(red: 0.3803921569, green: 0.6, blue: 0.6928837435, alpha: 1)
+        label1.font = UIFont(name: "Arial", size: 22)
+        label1.font = UIFont.boldSystemFont(ofSize: 22)
+     
+        answerLabel1.text! = desc1
+        answerLabel1.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        answerLabel1.font = UIFont(name: "Arial", size: 15)
+      
+        answerLabel2.text! = desc2
+        answerLabel2.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        answerLabel2.font = UIFont(name: "Arial", size: 15)
+       
+       answerLabel3.text! = desc3
+        answerLabel3.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        answerLabel3.font = UIFont(name: "Arial", size: 15) 
     }
     
     func addPopup() {
