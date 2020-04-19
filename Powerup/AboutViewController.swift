a959099ea4f34be87e823c1790a751836bc735e5
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-04-19 16:30:16 +0530

    Updated About View

--- Powerup/AboutViewController.swift
+++ Powerup/AboutViewController.swift
@@ -8,42 +8,37 @@ class AboutViewController: UIViewController {
     }
 
 
-    @IBOutlet weak var label1: UILabel!
- 
-    @IBOutlet weak var answerLabel1: UILabel!
+    @IBOutlet weak var questionFirst: UILabel!
+    @IBOutlet weak var answerFirst: UILabel!
+    @IBOutlet weak var questionSec: UILabel!
+    @IBOutlet weak var answerSec: UILabel!
+    @IBOutlet weak var questionThird: UILabel!
+    @IBOutlet weak var answerThird: UILabel!
     
-    @IBOutlet weak var answerLabel2: UILabel!
-    
-    @IBOutlet weak var answerLabel3: UILabel!
     
     override func viewDidAppear(_ animated: Bool) {
         addPopup()
-       // text(desc1: aboutText, desc2: aboutText2, desc3: aboutText3)
-        text(desc1: aboutText, desc2: aboutText2, desc3: aboutText3)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+         text(textFirst: aboutAnswerFirst, textSec: aboutAnswerSec, textThird: aboutAnswerThird)
+
     }
 
     // function for modification of about view taking string i/p from string and print to about view.
-    func text(desc1: String, desc2: String, desc3: String){
-        label1.text! = "The Game"
-        label1.textColor? = #colorLiteral(red: 0.3803921569, green: 0.6, blue: 0.6928837435, alpha: 1)
-        label1.font = UIFont(name: "Arial", size: 22)
-        label1.font = UIFont.boldSystemFont(ofSize: 22)
-     
-        // attributes for answer1
-        answerLabel1.text! = desc1
-        answerLabel1.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        answerLabel1.font = UIFont(name: "Arial", size: 15)
-      
-         // attributes for answer
-        answerLabel2.text! = desc2
-        answerLabel2.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        answerLabel2.font = UIFont(name: "Arial", size: 15)
-       
-         // attributes for answer3
-        answerLabel3.text! = desc3
-        answerLabel3.textColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        answerLabel3.font = UIFont(name: "Arial", size: 15) 
+    func text(textFirst: String, textSec: String, textThird: String){
+        
+        questionFirst.text = "The Game"
+        questionSec.text = "Why is PowerUp needed?"
+        questionThird.text = "How does PowerUp helps teenagers?"
+        
+        // attributes for answers
+        answerFirst.text! = textFirst
+        answerSec.text! = textSec
+        answerThird.text! = textThird
+        
     }
     
     func addPopup() {
